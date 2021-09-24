6f1c66954ecb4d642b93089f201acdf56f119ceb
Breno Machado <breno.machado.ti@outlook.com>
2021-09-24 01:34:20 -0300

    Created interface CompanyServices

--- services/companyService/companyServices.go
+++ services/companyService/companyServices.go
@@ -4,14 +4,6 @@ import (
 	"bmachado/Boaz.Api.Go/domain/entities"
 )
 
-type companyServiceInterface interface {
-	Add(company *entities.Company) (*entities.Company, error)
-	Delete(id string) error
-	Update(company *entities.Company)
-	GetAll() ([]*entities.Company, error)
-	Get(id int) (*entities.Company, error)
-}
-
 type companyService struct {
 	companyServiceInterface
 }
---
+++ services/companyService/interfaces.go
@@ -0,0 +1,11 @@
+package Services
+
+import "bmachado/Boaz.Api.Go/domain/entities"
+
+type companyServiceInterface interface {
+	Add(company *entities.Company) (*entities.Company, error)
+	Delete(id string) error
+	Update(company *entities.Company)
+	GetAll() ([]*entities.Company, error)
+	Get(id int) (*entities.Company, error)
+}
